0c961be9498da58aa0ae118aa138c8bf07317355
ValSua <valeria.ibarra@correounivalle.edu.co>
2025-04-12 16:39:38 -0500

    add providers

--- Front/catalogo_de_productos/src/app/app.config.ts
+++ Front/catalogo_de_productos/src/app/app.config.ts
@@ -1,8 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideToastr } from "ngx-toastr";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideHttpClient(withInterceptorsFromDi()),
+    provideToastr({
+      positionClass: 'toast-top-right',
+      timeOut: 3500,
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideAnimationsAsync()]
 };
