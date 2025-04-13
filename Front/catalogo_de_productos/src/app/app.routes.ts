84eeb61e755ec87a094ee7e775c16483fbb0969e
ValSua <valeria.ibarra@correounivalle.edu.co>
2025-04-12 21:31:16 -0500

    add create producto component

--- Front/catalogo_de_productos/src/app/app.routes.ts
+++ Front/catalogo_de_productos/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './components/principal/app.component';
 import { ListProductosComponent } from './components/productos/list-productos/list-productos.component';
 import { HomeComponent } from './components/home/home.component';
+import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
+import { UpdateProductoComponent } from './components/productos/update-producto/update-producto.component';
 
 export const routes: Routes = [
     {path: 'app-root', component: AppComponent},
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'list-productos', component: ListProductosComponent},
+    {path: 'createProducto', component: CreateProductoComponent},
+    {path: 'updateProducto/:id', component: UpdateProductoComponent},
 ];
--- Front/catalogo_de_productos/src/app/components/productos/create-producto/create-producto.component.ts
+++ Front/catalogo_de_productos/src/app/components/productos/create-producto/create-producto.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CreateProductoDto } from '../../../models/producto/create-producto-dto';
+import { ProductoService } from '../../../services/producto/producto.service';
 
 @Component({
   selector: 'app-create-producto',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatDialogModule
+  ],
   templateUrl: './create-producto.component.html',
   styleUrl: './create-producto.component.css'
 })
-export class CreateProductoComponent {
+export class CreateProductoComponent implements OnInit{
 
+  loading: boolean = true; 
+  listPath = 'list-productos';
+  error: string | null = null;
+  createProducto: CreateProductoDto = { 
+    productoId: 0,
+    nombre: '',
+    descripcion: '',
+    precio: 0,
+    stock: 0
+  };
+
+  constructor(
+    private productoService: ProductoService, private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.loading = false; 
+  }
+
+  isLoading: WritableSignal<boolean> = signal(true);
+
+  guardarCambios() {
+    this.productoService.createProducto(this.createProducto).subscribe({
+      next: () => {
+        alert('Producto creado correctamente');
+        this.router.navigate([this.listPath]);
+      },
+      error: (err) => {
+        this.error = 'Error al crear el producto.';
+        console.error(err);
+      },
+    });
+  }
+
+  cancelar() {
+    this.router.navigate([this.listPath]);
+  }
 }
