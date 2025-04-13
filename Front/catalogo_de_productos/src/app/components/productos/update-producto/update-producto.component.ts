1cdc44c44380b2d9a88bd9745f2b31181dacdaba
ValSua <valeria.ibarra@correounivalle.edu.co>
2025-04-12 21:31:43 -0500

    add update producto component

--- Front/catalogo_de_productos/src/app/components/productos/update-producto/update-producto.component.ts
+++ Front/catalogo_de_productos/src/app/components/productos/update-producto/update-producto.component.ts
@@ -1,11 +1,107 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { GetProductoDto } from '../../../models/producto/get-producto-dto';
+import { ProductoService } from '../../../services/producto/producto.service';
+import { UpdateProductoDto } from '../../../models/producto/update-producto-dto';
 
 @Component({
   selector: 'app-update-producto',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatDialogModule
+  ],
   templateUrl: './update-producto.component.html',
   styleUrl: './update-producto.component.css'
 })
-export class UpdateProductoComponent {
+export class UpdateProductoComponent implements OnInit {
 
+  loading: boolean = false;
+  listPath = 'list-productos';
+  error: string | null = null;
+  producto: GetProductoDto | null = null;
+  productoId!: number;
+  updateProducto!: UpdateProductoDto;
+
+  constructor(
+    private productoService: ProductoService, private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      const parsedId = parseInt(id, 10); 
+
+      if (!isNaN(parsedId)) {
+        this.getProductoById(parsedId); 
+      } else {
+        this.error = 'ID del producto no proporcionado';
+        this.loading = false;
+        this.isLoading.set(false);
+      }
+    }
+  }
+
+  isLoading: WritableSignal<boolean> = signal(true);
+
+  getProductoById(id: number): void {
+    this.loading = true;
+    this.error = null;
+
+    this.productoService.getProductoById(id).subscribe({
+      next: (response: GetProductoDto) => {
+        this.producto = response;
+
+        this.productoId = response.productoId;
+
+        this.updateProducto = {
+          productoId: response.productoId,
+          nombre: response.nombre,
+          descripcion: response.descripcion,
+          precio: response.precio,
+          stock: response.stock
+        };
+
+        this.loading = false;
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        if (err.status === 404) {
+          this.error = 'Producto no encontrado';
+        } else {
+          this.error = 'Error al cargar el producto';
+        }
+        this.loading = false;
+        this.isLoading.set(false);
+        console.error('Error en la solicitud:', err);
+      },
+    });
+  }
+
+  guardarCambios() {
+    if (!this.productoId) {
+      this.error = 'ID del producto no válido.';
+      return;
+    }
+
+    this.productoService.updateProducto(this.productoId, this.updateProducto).subscribe({
+      next: () => {
+        alert('Producto actualizado correctamente');
+        this.router.navigate([this.listPath]);
+      },
+      error: (err) => {
+        this.error = 'Error al actualizar el producto.';
+        console.error(err);
+      },
+    });
+  }
+
+  cancelar() {
+    this.router.navigate([this.listPath]);
+  }
 }
